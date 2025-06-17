bd3f77566da055b1b64e1e83b358a3a33e4cda07
alexandros <homocalidus@gmail.com>
2025-06-17 18:38:12 +0200

    members_uid get, put and delete routes added

---
+++ src/app/protected/api/members_uid/[id]/route.ts
@@ -0,0 +1,164 @@
+import { createClient } from "@/lib/server";
+import { members_uid_put, members_uid_single } from "@/lib/zod/members_uid";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const supabase = await createClient();
+  const { id: member_uid } = await params;
+
+  /*const authResult = await supabase.auth.getUser();
+
+  if (!authResult.data.user) {
+    return NextResponse.json(
+      { error: "Unauthorized request" },
+      { status: 401 }
+    );
+  }*/
+
+  const idValidationResult = members_uid_single.safeParse({ member_uid });
+
+  if (!idValidationResult.success) {
+    return NextResponse.json(
+      {
+        error: "Invalid parameter",
+        details: idValidationResult.error.flatten(),
+      },
+      { status: 400 }
+    );
+  }
+
+  const { data, error } = await supabase
+    .from("members_uid")
+    .select(
+      `
+      id,
+      identifier,
+      email,
+      date_of_birth,
+      date_of_joining,
+      date_of_leaving,
+      is_public,
+      membership_status_uid
+    `
+    )
+    .eq("id", idValidationResult.data.member_uid)
+    .maybeSingle();
+
+  if (error) {
+    return NextResponse.json(
+      { error: error.message || "Internal server error" },
+      { status: 500 }
+    );
+  }
+  if (!data) {
+    return NextResponse.json({ error: "Not found" }, { status: 404 });
+  }
+
+  return NextResponse.json({ data });
+}
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const supabase = await createClient();
+
+  const authResult = await supabase.auth.getUser();
+
+  if (!authResult.data.user) {
+    return NextResponse.json(
+      { error: "Unauthorized request" },
+      { status: 401 }
+    );
+  }
+
+  const { id: member_uid } = await params;
+
+  const body = await request.json();
+
+  const idValidationResult = members_uid_single.safeParse({ member_uid });
+
+  if (!idValidationResult.success) {
+    return NextResponse.json(
+      {
+        error: "Invalid parameter",
+        details: idValidationResult.error.flatten(),
+      },
+      { status: 400 }
+    );
+  }
+
+  const bodyValidationResult = members_uid_put.safeParse(body);
+
+  if (!bodyValidationResult.success) {
+    return NextResponse.json(bodyValidationResult.error, { status: 400 });
+  }
+
+  const updatedData = await supabase
+    .from("members_uid")
+    .update(bodyValidationResult.data)
+    .eq("id", idValidationResult.data.member_uid)
+    .select()
+    .single();
+
+  if (updatedData.error) {
+    return NextResponse.json(
+      updatedData.error || { error: "Update action failed" },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json({ ...updatedData.data }, { status: 200 });
+}
+
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const supabase = await createClient();
+
+  /*const authResult = await supabase.auth.getUser();
+
+  if (!authResult.data.user) {
+    return NextResponse.json(
+      { error: "Unauthorized request" },
+      { status: 401 }
+    );
+  }*/
+
+  const { id: member_uid } = await params;
+
+
+
+  const idValidationResult = members_uid_single.safeParse({ member_uid });
+
+  if (!idValidationResult.success) {
+    return NextResponse.json(
+      {
+        error: "Invalid parameter",
+        details: idValidationResult.error.flatten(),
+      },
+      { status: 400 }
+    );
+  }
+
+  
+
+  const deletedData = await supabase
+  .from('members_uid')
+  .delete()
+  .eq('id', idValidationResult.data.member_uid)
+
+  if (deletedData.error) {
+    return NextResponse.json(
+      deletedData.error || { error: "Delete action failed" },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json(null, { status: 200 });
+}
--- src/lib/zod/members_uid.ts
+++ src/lib/zod/members_uid.ts
@@ -18,7 +18,23 @@ export const members_uid_post = z.object({
   is_public: z.coerce.number().default(0),
 });
 
-export const members_uid_put = members_uid_post.extend({ id: z.coerce.number() });
+export const members_uid_put = z.object({
+  identifier: z.string().min(1).optional(),
+  email: z.string().email("Invalid email address").optional(),
+  date_of_birth: z
+    .string()
+    .regex(/^\d{4}-\d{2}-\d{2}$/, "Date must be in yyyy-mm-dd format")
+    .optional(),
+  date_of_joining: z
+    .string()
+    .regex(/^\d{4}-\d{2}-\d{2}$/, "Date must be in yyyy-mm-dd format").optional(),
+  date_of_leaving: z
+    .string()
+    .regex(/^\d{4}-\d{2}-\d{2}$/, "Date must be in yyyy-mm-dd format")
+    .optional(),
+  membership_status_uid: z.coerce.number().optional(),
+  is_public: z.coerce.number().optional(),
+});
 
 export const members_uid_get = z.object({
   identifier: z.string().min(1).optional(),
@@ -27,6 +43,10 @@ export const members_uid_get = z.object({
   is_public: z.coerce.number().optional(),
   offset: z.coerce.number().min(0).default(0),
   limit: z.coerce.number().min(5).max(30).default(15),
+});
+
+export const members_uid_single = z.object({
+  member_uid: z.coerce.number().int().positive(), // coerce string to number and validate it's a positive integer
 });
 
 export const members_uid_delete = z.object({ id: z.coerce.number() });
