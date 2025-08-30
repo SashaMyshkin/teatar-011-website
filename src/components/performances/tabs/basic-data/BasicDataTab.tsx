da16c397ad7887d551b96742751e9653a5bf1a86
alexandros <homocalidus@gmail.com>
2025-08-30 17:21:16 +0200

    cancel button

--- src/components/performances/tabs/basic-data/BasicDataTab.tsx
+++ src/components/performances/tabs/basic-data/BasicDataTab.tsx
@@ -2,11 +2,12 @@ import { format } from "date-fns";
 import { BasicInfoForm as BasicInfoFormType } from "@components/performances/types";
 import { useFormReducer } from "@/components/custom-hooks/useFormReducer";
 import { useFieldValidator } from "@/components/custom-hooks/validators";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { basicInfoFormValidation } from "@/lib/zod/performances/basic-info-form";
 import { usePerformanceContext } from "@components/performances/context/PerformanceContext";
 import BasicInfoForm from "@components/performances/forms/BasicInfoForm";
 import { Box, Button } from "@mui/material";
+import _ from "lodash";
 
 const initialFormState: BasicInfoFormType = {
   title: "",
@@ -23,30 +24,69 @@ export default function BasicDataTab() {
     initialFormState,
     basicInfoFormValidation
   );
+  const [dbState, setDbState] = useState<BasicInfoFormType>(initialFormState);
   //const [submitting, setSubmitting] = useState(false);
   const { performanceUid, performance } = usePerformanceContext();
 
   useEffect(() => {
     if (performanceUid && performance) {
-      resetFormState({
+      const data = {
         title: performance.title,
         identifier: performanceUid.identifier,
         date_of_premiere: format(performanceUid.date_of_premiere, "yyyy-MM-dd"),
         performance_type_uid: performanceUid.performance_type_uid,
         slogan: performance.slogan,
-      });
+      };
+      resetFormState(data);
+      setDbState(data);
     }
-  }, [performanceUid, performance, resetFormState]);
+  }, [performanceUid, performance]);
+
+  const handleCancel = () => resetFormState(dbState);
 
   return (
-    <Box sx={{ display: "flex",flexDirection:"column", justifyContent: "center", gap:"1rem", width:"25rem", margin:"auto" }} component="form">
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        gap: "1rem",
+        width: "25rem",
+        margin: "auto",
+      }}
+      component="form"
+    >
       <BasicInfoForm
         formState={formState}
         errorState={errorState}
         setField={setField}
         validateField={validateField}
       />
-      { performanceUid && performance && <Button variant="contained" fullWidth>Sačuvaj</Button>}
+      {performanceUid && performance && (
+        <Box sx={{display:"flex", gap:"0.9rem"}}>
+          <Button
+          variant="contained"
+          fullWidth
+          disabled={_.isEqual(dbState, formState)}
+          color="primary"
+          type="submit"
+          sx={{flexGrow:1}}
+        >
+          Sačuvaj
+        </Button>
+        <Button
+          variant="contained"
+          fullWidth
+          type="button"
+          color="secondary"
+          sx={{flexGrow:1}}
+          onClick={handleCancel}
+        >
+          Otkaži
+        </Button>
+        </Box>
+        
+      )}
     </Box>
   );
 }
