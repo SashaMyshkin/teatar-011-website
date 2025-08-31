e50775a9e1c9733f8750f11cf537f855f566f811
alexandros <homocalidus@gmail.com>
2025-09-01 00:21:04 +0200

    loading button

--- src/components/performances/tabs/performance-gallery/PerformanceGallery.tsx
+++ src/components/performances/tabs/performance-gallery/PerformanceGallery.tsx
@@ -1,13 +1,14 @@
 import { useSelectImages } from "@/components/image-manager/hooks/useSelectImages";
 import { usePerformanceContext } from "@components/performances/context/PerformanceContext"
 import Gallery from "@/components/image-manager/gallery/Gallery";
+import Loading from "@/components/loading/Loading";
 
 export default function PerformanceGallery(){
   const {performanceUid} = usePerformanceContext();
   const {imageData} = useSelectImages({entity_id:performanceUid?.id,type:"image-gallery"});
 
-  if(!imageData) return <></>
-  if(!performanceUid) return <></>
+  if(!imageData) return <Loading></Loading>
+  if(!performanceUid) return <Loading></Loading>
 
 
   return <Gallery images={imageData} futurePath={`performances/${performanceUid.identifier}/gallery`} entityId={performanceUid.id} />
